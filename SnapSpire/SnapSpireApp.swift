fe7d60905e884a7eccbb699762fb63f848d41810
Vlad Stanescu <vladstanescu94@gmail.com>
2023-02-28 16:01:52 +0200

    Add project files

---
+++ SnapSpire/SnapSpireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSpireApp.swift
+//  SnapSpire
+//
+//  Created by Vlad Stanescu on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnapSpireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
