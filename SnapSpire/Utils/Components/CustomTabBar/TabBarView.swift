cee52cf9ebee2f4fe08f4e6d545476487cd033da
Camelia <cameliabraghes@gmail.com>
2023-03-16 12:36:18 +0200

    Add asstes for tabBar

---
+++ SnapSpire/Utils/Components/CustomTabBar/TabBarView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
