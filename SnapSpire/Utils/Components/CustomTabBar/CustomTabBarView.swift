8d95669b65658de1f728d4151ba044a78bdf4ef0
Camelia <cameliabraghes@gmail.com>
2023-03-17 09:49:57 +0200

    Create custom tab bar

---
+++ SnapSpire/Utils/Components/CustomTabBar/CustomTabBarView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+// swiftlint:disable
+
+struct CustomTabBarView: View {
+    @Binding var selectedTab: TabItem
+
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(TabItem.allCases, id: \.self) { tab in
+                    HStack {
+                        withAnimation(.easeIn) {
+                            HStack {
+                                Image(selectedTab == tab ? tab.iconSelected : tab.rawValue)
+                                    .scaleEffect(selectedTab == tab ? 1.25 : 1)
+                                    .onTapGesture {
+                                        selectedTab = tab
+                                    }
+                            }
+                            .padding(.horizontal)
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .background(.thinMaterial)
+        }
+    }
+}
+
+struct CustomTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBarView(selectedTab: .constant(.home))
+    }
+}
---
+++ SnapSpire/Utils/Components/CustomTabBar/TabItem.swift
@@ -0,0 +1,8 @@
+//
+//  TabItem.swift
+//  SnapSpire
+//
+//  Created by Camelia Braghes on 16.03.2023.
+//
+
+import Foundation
