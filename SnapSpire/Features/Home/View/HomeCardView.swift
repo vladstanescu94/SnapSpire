6ceb5d340a1a172516c12f321578180476b7c320
Camelia <cameliabraghes@gmail.com>
2023-03-21 12:01:22 +0200

    Create home card view

--- SnapSpire/Features/Home/View/HomeCardView.swift
+++ SnapSpire/Features/Home/View/HomeCardView.swift
@@ -1,13 +1,59 @@
 import SwiftUI
 
 struct HomeCardView: View {
+    let user: User
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image(user.userProfileImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30)
+                    .foregroundColor(.red)
+                    .clipShape(Circle())
+
+                Text(user.userName)
+                    .customFont(font: .rubikItalic, size: 16)
+                    .foregroundColor(Color("primaryText"))
+                    .shadow(radius: 4, y: 4)
+
+                Spacer()
+                Text(user.timePosting)
+                    .customFont(font: .rubikItalic)
+                    .foregroundColor(Color("placeholder"))
+            }
+            .padding(.horizontal, 8)
+
+            Image(user.userPostImage)
+            HStack {
+                Image("add")
+                Spacer()
+
+                Text("\(user.numberComments)")
+                    .customFont(font: .rubikItalic)
+                    .foregroundColor(Color("secondaryText"))
+                Image("chat")
+
+                Text("\(user.numberLikes)")
+                    .customFont(font: .rubikItalic)
+                    .foregroundColor(Color("secondaryText"))
+                Image("heart")
+            }
+            .padding(.horizontal, 8)
+        }
+        .padding(.vertical, 16)
+        .background {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.white)
+        }
+        .padding(.horizontal, 32)
     }
 }
 
 struct HomeCardView_Previews: PreviewProvider {
+    static var user = User.sampleData
     static var previews: some View {
-        HomeCardView()
+        HomeCardView(user: user[0])
     }
 }
