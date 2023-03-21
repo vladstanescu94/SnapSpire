c8ac35b542f3ecfbe707caaaeca12772c5ba1bee
Camelia <cameliabraghes@gmail.com>
2023-03-21 12:00:46 +0200

    Add user model

---
+++ SnapSpire/Features/Home/Model/User.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct User {
+    var userProfileImage: String
+    var userName: String
+    var userPostImage: String
+    var timePosting: String
+    var numberComments: Int = 0
+    var numberLikes: Int = 0
+}
+
+extension User {
+    static let sampleData: [User] = [
+        User(userProfileImage: "Avatar", userName: "Thanh Pham", userPostImage: "Photo", timePosting: "1 hour ago"),
+        User(userProfileImage: "Avatar", userName: "Thanh Pham", userPostImage: "Photo", timePosting: "1 hour ago"),
+        User(userProfileImage: "Avatar", userName: "Thanh Pham", userPostImage: "Photo", timePosting: "1 hour ago"),
+    ]
+}
