e2a770fe78349f389accb71b3a9f543d35057c65
Camelia <cameliabraghes@gmail.com>
2023-03-29 14:25:32 +0300

    Add a color extension.

---
+++ SnapSpire/Utils/Extensions/Color+Extension.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+extension Color {
+    static let backgroundField = Color("backgroundField")
+    static let backgroundGrey = Color("backgroundGrey")
+    static let backgroundOverlay = Color("backgroundOverlay")
+    static let primaryBackground = Color("primaryBackground")
+
+    static let error = Color("error")
+    static let primary = Color("primary")
+
+    static let placeholder = Color("placeholder")
+    static let primaryText = Color("primaryText")
+    static let secondaryText = Color("secondaryText")
+    static let tertiaryText = Color("tertiaryText")
+}
