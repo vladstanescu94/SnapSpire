d1766bfe82a1884949d9a0dd9b77bbadf2e6c238
Camelia <cameliabraghes@gmail.com>
2023-06-08 11:54:55 +0300

    Create Discovery View

---
+++ SnapSpire/Features/Discover/View/DiscoverView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverView.swift
+//  SnapSpire
+//
+//  Created by Camelia Braghes on 08.06.2023.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiscoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoverView()
+    }
+}
