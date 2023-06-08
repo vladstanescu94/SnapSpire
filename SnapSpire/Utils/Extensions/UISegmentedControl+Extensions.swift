1271d2c37deb31dcedb70e6496df7fa253b80af1
Camelia <cameliabraghes@gmail.com>
2023-06-08 21:07:49 +0300

    Fix swiftlint warnings

--- SnapSpire/Utils/Extensions/UISegmentedControl+Extensions.swift
+++ SnapSpire/Utils/Extensions/UISegmentedControl+Extensions.swift
@@ -1,9 +1,7 @@
 import SwiftUI
 
 extension UISegmentedControl {
-    func defaultConfig(font: UIFont = UIFont.italicSystemFont(ofSize: UIConstants.mediumSize),
-                       color: UIColor = UIColor(.placeholder))
-    {
+    func defaultConfig(font: UIFont = UIFont.italicSystemFont(ofSize: 16), color: UIColor = UIColor(.placeholder)) {
         let defaultAttributes = [
             NSAttributedString.Key.font: font,
             NSAttributedString.Key.foregroundColor: color,
@@ -11,9 +9,7 @@ extension UISegmentedControl {
         setTitleTextAttributes(defaultAttributes, for: .normal)
     }
 
-    func selectedConfig(font: UIFont = UIFont.boldSystemFont(ofSize: UIConstants.mediumSize),
-                        color: UIColor = UIColor(.primary))
-    {
+    func selectedConfig(font: UIFont = UIFont.boldSystemFont(ofSize: 16), color: UIColor = UIColor(.primary)) {
         let selectedAttributes = [
             NSAttributedString.Key.font: font,
             NSAttributedString.Key.foregroundColor: color,
